598a2fc5d339f49f3ef34a799d211604eaaf5aa8
RooRsmoka <chri.disa96@gmail.com>
2021-12-13 17:28:42 +0100

    esercizio 1 (Mail) completato

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
-// Esercizio 1
+// Esercizio 1(Mail)
 // Programma che chiede all'utente la sua email, 
 // controlla che sia sulla lista di chi può accedere
 // e stampa un messaggio a seconda dell'esito del controllo.
 
+// definisco un array in cui metterò una lista di email.
+const emailVerified = ['example@gmail.com','example@gmail.it', 'example@outlook.it', 'example@hotmail.it']
+// Definisco una variabile 'flag = false' in modo che, l'utente acceda solo
+// quando immette un email identica a quelle contenute nell'array.
+let flag = false;
 // Chiedo all'utente la sua email.
+const userEmail = prompt('Inserisci la tua email.');
 // Controllo se l'email inserita è presente sulla lista delle email che hanno accesso,
+for (let i = 0; i < emailVerified.length; i++) {
+    
+    if (userEmail === emailVerified[i]) {
+        flag = true;
+    }
+};
 // se il controllo risulta positivo stamperò un messaggio di benvenuto,
-// altrimenti stamperò un messaggio di email errata.
+// altrimenti stamperò un messaggio di email errata/non registrata.
+if (flag) {
+    console.log('Hai effettuato l\'accesso.')
+} else {
+    console.log('Questa email non risulta registrata.')
+};
 
+/********************************************************/
 
-
-// Esercizio 2
+// Esercizio 2(Gioco dei dadi)
 // Programma che genera un numero randomico da 1 a 6
 // sia per il player umano che per il computer,
 // dopodichè stabilisce un vincitore basandosi

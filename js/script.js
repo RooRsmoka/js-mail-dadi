cb20d2abc2ef8b86086a05dfb77964bda1fa58fc
RooRsmoka <chri.disa96@gmail.com>
2021-12-13 18:19:44 +0100

    esercizio 2(Gioco dei dadi) completato

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // e stampa un messaggio a seconda dell'esito del controllo.
 
 // definisco un array in cui metterò una lista di email.
-const emailVerified = ['example@gmail.com','example@gmail.it', 'example@outlook.it', 'example@hotmail.it']
+const emailVerified = ['example@gmail.com','example@gmail.it', 'example@outlook.it', 'example@hotmail.it'];
 // Definisco una variabile 'flag = false' in modo che, l'utente acceda solo
 // quando immette un email identica a quelle contenute nell'array.
 let flag = false;
@@ -34,7 +34,22 @@ if (flag) {
 // sul confronto dei valori dei numeri generati.
 
 // Genero un numero casuale per il player umano.
+let humanNumber = Math.floor(Math.random(1) * 6) + 1; 
 // Genero un numero casuale per il computer.
+let computerNumber = Math.floor(Math.random(1) * 6) + 1;
+
+console.log(humanNumber);
+console.log(computerNumber);
 // Controllo se il numero generato dal player umano
 // è maggiore, uguale o minore del numero generato dal computer(o viceversa)
 // A seconda dei vari casi stamperò un messaggio appropriato.
+if (humanNumber > computerNumber) {
+    console.log('hai vinto!')
+} else if (humanNumber < computerNumber) {
+    console.log('ha vinto il computer.')
+} else {
+    console.log('è un pareggio.')
+};
+
+
+

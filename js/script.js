c1d325c684010092cf1090aa69864909fc69c1e9
RooRsmoka <chri.disa96@gmail.com>
2021-12-13 16:38:58 +0100

    creazione file e commenti per sviluppo

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Esercizio 1
+// Programma che chiede all'utente la sua email, 
+// controlla che sia sulla lista di chi può accedere
+// e stampa un messaggio a seconda dell'esito del controllo.
+
+// Chiedo all'utente la sua email.
+// Controllo se l'email inserita è presente sulla lista delle email che hanno accesso,
+// se il controllo risulta positivo stamperò un messaggio di benvenuto,
+// altrimenti stamperò un messaggio di email errata.
+
+
+
+// Esercizio 2
+// Programma che genera un numero randomico da 1 a 6
+// sia per il player umano che per il computer,
+// dopodichè stabilisce un vincitore basandosi
+// sul confronto dei valori dei numeri generati.
+
+// Genero un numero casuale per il player umano.
+// Genero un numero casuale per il computer.
+// Controllo se il numero generato dal player umano
+// è maggiore, uguale o minore del numero generato dal computer(o viceversa)
+// A seconda dei vari casi stamperò un messaggio appropriato.
